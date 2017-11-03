4490c7ba936a073f1cceb66c08804b26d7ef62e0
ahmadfahmip <afahmi3@yahoo.co.id>
2017-11-03 10:44:12 +0700

    Update Shape.py
    add rotate + matrix multiplication

--- Shape.py
+++ Shape.py
@@ -16,6 +16,15 @@ class Shape:
     def _update_matrix(self, i):
         self.point_matrix_old[i][0] = self.point_matrix[i][0]
         self.point_matrix_old[i][1] = self.point_matrix[i][1]
+    
+    def mulMatrix(self, listA,listB):
+        res = []
+        for i in range(len(listA)):
+            tmp = 0
+            for j in range(len(listB)):
+                tmp += listA[j][i] * listB[j]
+                res.append(tmp)
+        return res
 
     def translate(self, dx, dy):
         for i in range(self.num_of_point):
@@ -28,6 +37,17 @@ class Shape:
             self._update_matrix(i)
             self.point_matrix[i][0] *= zoom
             self.point_matrix[i][1] *= zoom
+            
+    def rotate(self, deg, x, y):
+        matrix = []
+        rad = math.radians(deg)
+        matrix.append([float(math.cos(rad)), float(math.sin(rad))])
+        matrix.append([float(math.sin(rad) * -1), float(math.cos(rad))])
+        for i in range(self.num_of_point):
+            res = mulMatrix(matrix, trans[i])
+            self._update_matrix(i)
+            self.point_matrix[i][0] = res[0]
+            self.point_matrix[i][1] = res[1]
 
     def print_point(self):
         for x in self.point_matrix:
